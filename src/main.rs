2cbaa48c5eaa009f8ee9ba36bbe6f8fdafd07ae3
Arkitu <maxime@tlon.fr>
2023-09-07 10:52:23 +0000

    ça marche

--- src/main.rs
+++ src/main.rs
@@ -12,38 +12,20 @@ impl Drop for CleanUp {
 }
 
 fn char_to_number(c: char) -> u8 {
-    match c {
-        'a' => 0,
-        'b' => 1,
-        'c' => 2,
-        'd' => 3,
-        'e' => 4,
-        'f' => 5,
-        'g' => 6,
-        'h' => 7,
-        'i' => 8,
-        'j' => 9,
-        'k' => 10,
-        'l' => 11,
-        'm' => 12,
-        'n' => 13,
-        'o' => 14,
-        'p' => 15,
-        'q' => 16,
-        'r' => 17,
-        's' => 18,
-        't' => 19,
-        'u' => 20,
-        'v' => 21,
-        'w' => 22,
-        'x' => 23,
-        'y' => 24,
-        'z' => 25,
-        _ => panic!("pas une lettre")
+    if !c.is_ascii_alphabetic() {
+        panic!("pas une lettre");
     }
+    (c as u8) - 97
 }
 fn chars_to_numbers(string: &str) -> Vec<u8> {
     string.chars().map(|c| char_to_number(c)).collect()
+}
+
+fn number_to_char(i: u8) -> char {
+    if i > 25 {
+        panic!("trop grand");
+    }
+    (i+97) as char
 }
 
 struct Rotor {
@@ -58,6 +40,13 @@ impl Rotor {
             get_count: 0,
             max_count
         }
+    }
+    pub fn default_rotors() -> [Self; 3] {
+        [
+            Rotor::new(&chars_to_numbers("ekmflgdqvzntowyhxuspaibrcj"), 26*26),
+            Rotor::new(&chars_to_numbers("ajdksiruxblhwtmcqgznpyfvoe"), 26),
+            Rotor::new(&chars_to_numbers("bdfhjlcprtxvznyeiwgakmusqo"), 1)
+        ]
     }
     pub fn rotate(&mut self) {
         let n = self.matching.pop_back().unwrap();
@@ -82,12 +71,8 @@ fn main() -> Result<()> {
     let _clean_up = CleanUp;
     terminal::enable_raw_mode()?;
     execute!(stdout(), cursor::Hide)?;
-    let mut last_in: Option<char> = None;
-    let rotors = [
-        Rotor::new(&chars_to_numbers("ekmflgdqvzntowyhxuspaibrcj"), 26*26),
-        Rotor::new(&chars_to_numbers("ajdksiruxblhwtmcqgznpyfvoe"), 26),
-        Rotor::new(&chars_to_numbers("bdfhjlcprtxvznyeiwgakmusqo"), 1)
-    ];
+    let mut lit_up: Option<char> = None;
+    let mut rotors = Rotor::default_rotors()
     loop {
         let mut text = format!("
             A Z E R T Y U I O P\r
@@ -100,8 +85,24 @@ fn main() -> Result<()> {
                     event::KeyEvent {
                         code: KeyCode::Char(c),
                         ..
-                    } => if c.is_alphabetic() {
-                        last_in = Some(c.to_uppercase().next().unwrap())
+                    } => if c.is_ascii_alphabetic() {
+                        let c = c.to_lowercase().next().unwrap();
+                        let mut i = char_to_number(c);
+                        for rotor in rotors.iter_mut() {
+                            println!("{}", i);
+                            i = rotor.get(i, false);
+                        }
+                        for rotor in rotors.iter_mut() {
+                            println!("{}", i);
+                            i = rotor.get(i, true);
+                        }
+                        lit_up = Some(number_to_char(i));
+                    },
+                    event::KeyEvent {
+                        code: KeyCode::Enter,
+                        ..
+                    } => {
+                        rotors = Rotor::default_rotors();
                     },
                     _ => {}
                 }
@@ -109,8 +110,8 @@ fn main() -> Result<()> {
         }
 
 
-
-        if let Some(c) = last_in {
+        if let Some(c) = lit_up {
+            let c = c.to_uppercase().next().unwrap();
             if let Some(splited) = text.split_once(c) {
                 text = splited.0.to_string() + &c.black().on_dark_yellow().to_string() + splited.1;
             }
